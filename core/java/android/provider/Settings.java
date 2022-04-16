5b0499040b23af65ce1c37632934f4c4dfe5d6dd
Pranav Vashi <neobuddy89@gmail.com>
2022-04-16 10:38:36 +0530

    Fixes and improvements for Black Theme
    * Add to SettingsObserver for observing toggle behavior.
    * Apply only with dark theme - looks awful on light mode.
    * Add configuration listener to reapply theme when dark mode changes.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Mayur <ultramayur123@gmail.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11469,6 +11469,10 @@ public final class Settings {
          * @hide
          */
         public static final String LOCKSCREEN_MEDIA_METADATA = "lockscreen_media_metadata";
+         * Whether to turn on black theme
+         * @hide
+         */
+        public static final String SYSTEM_BLACK_THEME = "system_black_theme";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
